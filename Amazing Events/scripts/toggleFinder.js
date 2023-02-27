64ad6a16876996444955ed597c2a423377877763
Michael Mena <michael.mena2408@gmail.com>
2023-02-27 14:50:46 -0300

    task-1

---
+++ Amazing Events/scripts/toggleFinder.js
@@ -0,0 +1,7 @@
+const finderLabel = document.getElementById("finderContainerLabel");
+const blurLabel = () => {
+    finderLabel.style.boxShadow = "0 0.2rem 0 0.2rem #d90268";
+}
+const focusLabel = () => {
+        finderLabel.style.boxShadow = "0 0 0 0.2rem #f9a826";
+}
